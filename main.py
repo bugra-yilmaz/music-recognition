70b88077b9609fd1203010dfe71beab57ebf88ce
bugra-yilmaz <bugra.yyilmaz@gmail.com>
2021-05-21 13:08:40 +0200

    Update demo

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import cv2 as cv
 
-from tools import detect_lines, reduce_objects
+from tools import detect_lines, reduce_objects, get_music, play
 from detection import get_musical_objects
 
 if __name__ == '__main__':
-    image_path = 'data/simple/02.png'
+    image_path = 'data/simple/04.png'
 
     image = cv.imread(image_path, 0)
     lines = detect_lines(image)
 
     objects = get_musical_objects(image_path)
 
-    notes = reduce_objects(objects, lines)
+    notes, groups = reduce_objects(objects, lines)
+
+    music = get_music(notes, lines)
+    
+    play(music)
