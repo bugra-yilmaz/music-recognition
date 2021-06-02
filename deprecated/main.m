dedaed24a9bb1b79f4a6e402f0e1d99881bb0289
bugra-yilmaz <bugra.yyilmaz@gmail.com>
2021-06-02 23:16:50 +0200

    Remove deprecated MATLAB code

--- deprecated/main.m
+++
@@ -1,7 +0,0 @@
-% Exemplary code to play Jingle Bells from a skewed sheet image
-% Get music sheet image
-image = imread('jingle_bells_skewed.jpg');
-% Find note positions on the sheet
-myNotes = findNotes(image);
-% Play the notes
-playNotes(myNotes);
